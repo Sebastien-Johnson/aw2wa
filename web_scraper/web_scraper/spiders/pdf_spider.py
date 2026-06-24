f83e6ff86d28fa052636cb104f32b92eb0f79ff1
Sebastien-Johnson <sebastien.z.johnson@gmail.com>
2026-06-24 00:20:54 -0400

    updated scraper to search through Nasa pages

--- web_scraper/web_scraper/spiders/pdf_spider.py
+++ web_scraper/web_scraper/spiders/pdf_spider.py
@@ -4,7 +4,7 @@ from urllib.parse import urljoin
 
 class PdfSpider(scrapy.Spider):
     name = "pdf_spider"
-    start_urls = ["https://drivenasa.com/results/?ee=1&eeFolder=NASA_Great_Lakes_Region%2F2025-Official-Results%2F2-Mid-Ohio-May-25&eeListID=2"]
+    start_urls = ["https://drivenasa.com/results/?ee=1&eeListID=2"]
 
     def parse(self, response):
         pdf_links = response.css('a[href$=".pdf"]::attr(href)').getall()
@@ -18,5 +18,6 @@ class PdfSpider(scrapy.Spider):
                 'tables' : []
             }         
 
-        for next_page in response.css('a[href*="page"]::attr(href)').getall():
+        for next_page in response.css('a[href*="results"]::attr(href)').getall():
+            print(next_page)
             yield response.follow(next_page, self.parse)
