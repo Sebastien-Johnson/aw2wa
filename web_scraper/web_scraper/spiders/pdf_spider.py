d922ec2c94959b21e491f28914cd08a7d5b2653a
Sebastien-Johnson <sebastien.z.johnson@gmail.com>
2026-06-29 01:32:47 -0400

    Filtered pdf files from next page requests

--- web_scraper/web_scraper/spiders/pdf_spider.py
+++ web_scraper/web_scraper/spiders/pdf_spider.py
@@ -4,12 +4,11 @@ from urllib.parse import urljoin
 
 class PdfSpider(scrapy.Spider):
     name = "pdf_spider"
-    start_urls = ["https://drivenasa.com/results/?ee=1&eeFolder=NASA_Great_Lakes_Region%2F2026-Official-Results%2F3-Mid-Ohio-May-26&eeListID=2"]
+    start_urls = ["https://drivenasa.com/results/?ee=1&eeListID=2"]
 
 
     def parse(self, response):
         pdf_links = response.css('a[href$=".pdf"]::attr(href)').getall()
-
         
         for link in pdf_links:
             pdf_url = urljoin(response.url, link)
@@ -18,6 +17,8 @@ class PdfSpider(scrapy.Spider):
             item["source_url"] = response.url
             yield item
 
-        for next_page in response.css( 'a[href*="results"]::attr(href)').getall():
-            print(next_page)
-            yield response.follow(next_page, self.parse)
+        for next_page in response.css('a[href*="results"]::attr(href)').getall():
+            if next_page[-4:] != ".pdf":
+                print(next_page)
+                yield response.follow(next_page, self.parse)
+        
